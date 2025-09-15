0e00e01cceb3554531251c69bb8996257f38cf93
DawnIsProblem <qweqwerty12321@gmail.com>
2025-09-15 15:04:13 +0900

    [baekjoon] 24723

---
+++ src/main/java/com/algorithm/bronze4/baekjoon_24723/Main.java
@@ -0,0 +1,21 @@
+package com.algorithm.bronze4.baekjoon_24723;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int result = (int)Math.pow(2, N);
+
+        sb.append(result);
+
+        System.out.print(sb);
+
+    }
+}
